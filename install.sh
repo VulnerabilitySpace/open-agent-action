1ce99f208101ec58896c137ab1cb2880290bf06c
owefsad <dongzhiyong@secnium.cn>
2022-01-03 15:27:54 +0800

    Debug: add profile print

--- install.sh
+++ install.sh
@@ -6,7 +6,15 @@ if [ "$DONGTAI_DEBUG"=="false" ];then
 else
     echo "debug mode"
 fi
+
+export JAVA_TOOL_OPTIONS="-javaagent:/tmp/agent.jar -Dproject.create=$PROJECT_CREATE -Dproject.name=$PROJECT_NAME -Dproject.version=$PROJECT_VERSION"
+
+echo "echo \"install DongTai to .bashrc\"" >> ~/.bashrc
+echo "export JAVA_TOOL_OPTIONS=\"-javaagent:/tmp/agent.jar -Dproject.create=$PROJECT_CREATE -Dproject.name=$PROJECT_NAME -Dproject.version=$PROJECT_VERSION\"" >> ~/.bashrc
+echo "echo \"install DongTai to .bashrc Successful\"" >> ~/.bashrc
+
+echo "echo \"install DongTai to .bash_profile\"" >> ~/.bash_profile
 echo "export JAVA_TOOL_OPTIONS=\"-javaagent:/tmp/agent.jar -Dproject.create=$PROJECT_CREATE -Dproject.name=$PROJECT_NAME -Dproject.version=$PROJECT_VERSION\"" >> ~/.bash_profile
-echo "Install Successful."
+echo "echo \"install DongTai to .bash_profile Successful\"" >> ~/.bash_profile
 
 echo "::set-output name=agent-env::$JAVA_TOOL_OPTIONS"
