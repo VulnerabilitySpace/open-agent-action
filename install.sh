3b8a43a59c6c00ccbf71d4593d960742fc012805
owefsad <dongzhiyong@secnium.cn>
2022-01-03 10:26:34 +0800

    Fix Inputs

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-echo "${{ inputs.DONGTAI_SERVER }}"
-echo "${{ inputs.DONGTAI_TOKEN }}"
-echo "${{ inputs.DONGTAI_DEBUG }}"
-echo "${{ inputs.PROJECT_NAME }}"
-echo "${{ inputs.PROJECT_CREATE }}"
-echo "${{ inputs.PROJECT_VERSION }}"
+echo "${{ INPUTS_DONGTAI_SERVER }}"
+echo "${{ INPUTS_DONGTAI_TOKEN }}"
+echo "${{ INPUTS_DONGTAI_DEBUG }}"
+echo "${{ INPUTS_PROJECT_NAME }}"
+echo "${{ INPUTS_PROJECT_CREATE }}"
+echo "${{ INPUTS_PROJECT_VERSION }}"
 
 # if debug, then skip this step
-curl -X GET "${{ inputs.DONGTAI_SERVER }}/api/v1/agent/download?url=https://iast.io/openapi&language=java" -H "Authorization: Token ${{ inputs.DONGTAI_TOKEN }}" -o /tmp/agent.jar -k
+curl -X GET "${{ INPUTS_DONGTAI_SERVER }}/api/v1/agent/download?url=https://iast.io/openapi&language=java" -H "Authorization: Token ${{ INPUTS_DONGTAI_TOKEN }}" -o /tmp/agent.jar -k
 
-export JAVA_TOOL_OPTIONS="-Dproject.create=${{ inputs.PROJECT_CREATE }} -Dproject.name=${{ inputs.PROJECT_NAME }} -Dproject.version=${{ inputs.PROJECT_VERSION }}"
+export JAVA_TOOL_OPTIONS="-Dproject.create=${{ INPUTS_PROJECT_CREATE }} -Dproject.name=${{ INPUTS_PROJECT_NAME }} -Dproject.version=${{ INPUTS_PROJECT_VERSION }}"
