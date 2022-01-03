d33a89af0f34e68d4a34ae621315d7a778afc706
owefsad <dongzhiyong@secnium.cn>
2022-01-03 11:29:19 +0800

    Test ENV

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 echo "Install DongTai For Project: $PROJECT_NAME"
 
-echo "$DONGTAI_SERVER"
-echo "$DONGTAI_TOKEN"
-echo "$DONGTAI_DEBUG"
-echo "$PROJECT_NAME"
-echo "$PROJECT_CREATE"
-echo "$PROJECT_VERSION"
-
 # if debug, then skip this step
-curl -X GET "$DONGTAI_SERVER/api/v1/agent/download?url=https://iast.io/openapi&language=java" -H "Authorization: Token $DONGTAI_TOKEN" -o /tmp/agent.jar -k
+if [ ! DONGTAI_DEBUG ];then
+    curl -X GET "$DONGTAI_SERVER/api/v1/agent/download?url=https://iast.io/openapi&language=java" -H "Authorization: Token $DONGTAI_TOKEN" -o /tmp/agent.jar -k
+else
+    echo "debug mode"
+fi
 
 export JAVA_TOOL_OPTIONS="-Dproject.create=$PROJECT_CREATE -Dproject.name=$PROJECT_NAME -Dproject.version=$PROJECT_VERSION"
 
