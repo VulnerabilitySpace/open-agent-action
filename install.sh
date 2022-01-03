04b2f62a2324a3af0fa2475a48b0e74de0596050
owefsad <dongzhiyong@secnium.cn>
2022-01-03 14:42:15 +0800

    Debug: add profile print

--- install.sh
+++ install.sh
@@ -8,6 +8,4 @@ else
 fi
 
 echo JAVA_TOOL_OPTIONS="\"-javaagent:/tmp/agent.jar -Dproject.create=$PROJECT_CREATE -Dproject.name=$PROJECT_NAME -Dproject.version=$PROJECT_VERSION\"" >> ~/.bash_profile
-source ~/.bash_profile
-echo "Install Successful."
 echo "::set-output name=agent-env::$JAVA_TOOL_OPTIONS"
