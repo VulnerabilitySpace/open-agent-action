b51b1f8b9acd2ac6162a9aec92230852630beac1
owefsad <dongzhiyong@secnium.cn>
2022-01-03 10:35:47 +0800

    Fix Inputs

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+echo "Install DongTai For Project: $INPUTS_PROJECT_NAME"
+
 echo "$INPUTS_DONGTAI_SERVER"
 echo "$INPUTS_DONGTAI_TOKEN"
 echo "$INPUTS_DONGTAI_DEBUG"
@@ -9,3 +11,5 @@ echo "$INPUTS_PROJECT_VERSION"
 curl -X GET "$INPUTS_DONGTAI_SERVER/api/v1/agent/download?url=https://iast.io/openapi&language=java" -H "Authorization: Token $INPUTS_DONGTAI_TOKEN" -o /tmp/agent.jar -k
 
 export JAVA_TOOL_OPTIONS="-Dproject.create=$INPUTS_PROJECT_CREATE -Dproject.name=$INPUTS_PROJECT_NAME -Dproject.version=$INPUTS_PROJECT_VERSION"
+
+echo "Install Finished. Project ENV is: $JAVA_TOOL_OPTIONS"
