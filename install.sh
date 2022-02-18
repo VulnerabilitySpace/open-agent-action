f8bbf16d01ada49d5950c2d844b589ffbd4a524d
owefsad <dongzhiyong@secnium.cn>
2022-02-18 12:42:39 +0800

    upgrade java agent argument

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ if [ "$DONGTAI_DEBUG"=="false" ];then
     curl -X GET "$DONGTAI_SERVER/api/v1/agent/download?url=https://iast.io/openapi&language=java" -H "Authorization: Token $DONGTAI_TOKEN" -o /tmp/agent.jar -k
 else
     echo "debug mode"
-fi
+fil
 
-export JAVA_TOOL_OPTIONS="-javaagent:/tmp/agent.jar -Ddongtai.app.create=$PROJECT_CREATE -Ddongtai.app.name=$PROJECT_NAME -Ddongtai.app.version=$PROJECT_VERSION"
+export JAVA_TOOL_OPTIONS="-javaagent:/tmp/agent.jar -Ddongtai.app.create=$PROJECT_CREATE -Ddongtai.app.name=$PROJECT_NAME -Ddongtai.app.version=$PROJECT_VERSION -Ddongtai.debug=$DONGTAI_DEBUG"
 
 echo "::set-output name=agent-env::$JAVA_TOOL_OPTIONS"
